f7eefbaf93a301507377466fb446feb9f212595e
Aleksandar Atanasov <107752460+aleks930819@users.noreply.github.com>
2022-12-08 11:35:09 +0200

    take the values from reg and login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,53 @@
-import { Link } from "react-router-dom";
-
-
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+  });
+
+  const changeHandler = (e) => {
+    setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+    e.value = e.target.value;
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+  };
+
   return (
     <div className="form-box">
-    <form className="form">
-      <h2>LOGIN</h2>
-      <div className="username">
-        {/* <label id="username">Username</label> */}
-        <input type="text" htmlFor="username"  placeholder="Username" name="username"/>
-      </div>
-      <div className="password">
-        {/* <label id="password">Password</label> */}
-        <input type="password"  htmlFor="password" placeholder="Password" name="password" />
-      </div>
-      <button className="form-btn" type="button">Login</button>
-      <Link to='/register'>Don't have an account? Register</Link>
-    </form>
+      <form className="form" onSubmit={submitHandler}>
+        <h2>LOGIN</h2>
+        <div className="username">
+          {/* <label id="username">Username</label> */}
+          <input
+            type="text"
+            htmlFor="username"
+            placeholder="Username"
+            value={values.username}
+            name="username"
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="password">
+          {/* <label id="password">Password</label> */}
+          <input
+            type="password"
+            htmlFor="password"
+            placeholder="Password"
+            value={values.password}
+            name="password"
+            onChange={changeHandler}
+          />
+        </div>
+        <button className="form-btn">
+          Login
+        </button>
+        <Link to="/register">Don't have an account? Register</Link>
+      </form>
     </div>
   );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+    repassword: '',
+  });
+
+  const changeHandler = (e) => {
+    setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+    e.value = e.target.value;
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="form-box">
-      <form className="form">
+      <form className="form" onSubmit={submitHandler}>
         <h2>REGISTER</h2>
         <div className="username">
           {/* <label id="username">Username</label> */}
@@ -12,6 +28,8 @@ const Register = () => {
             htmlFor="username"
             placeholder="Username"
             name="username"
+            value={values.username}
+            onChange={changeHandler}
           />
         </div>
         <div className="password">
@@ -21,6 +39,8 @@ const Register = () => {
             htmlFor="password"
             placeholder="Password"
             name="password"
+            value={values.password}
+            onChange={changeHandler}
           />
         </div>
 
@@ -31,12 +51,11 @@ const Register = () => {
             htmlFor="password"
             placeholder="Confirm Password"
             name="repassword"
-
+            value={values.repassword}
+            onChange={changeHandler}
           />
         </div>
-        <button className="form-btn" type="button">
-          Register
-        </button>
+        <button className="form-btn">Register</button>
 
         <Link to="/login">You already have an account? Login</Link>
       </form>
